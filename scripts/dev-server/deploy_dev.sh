d1ba75be17603b9e2e4e35cfa1043fbbf2db6f05
suabae <bsa0322@ewhain.net>
2024-03-27 02:10:01 +0900

    👷 add CI/CD

---
+++ scripts/dev-server/deploy_dev.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Installing docker engine if not exists
+if ! type docker > /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+# Installing docker-compose if not exists
+if ! type docker-compose > /dev/null
+then
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+  sudo mkdir -p $DOCKER_CONFIG/cli-plugins
+  sudo curl -SL https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+fi
+
+echo "start docker compose up: ubuntu"
+sudo docker compose -f /home/ubuntu/srv/project/docker-compose.dev.yml up --build -d
