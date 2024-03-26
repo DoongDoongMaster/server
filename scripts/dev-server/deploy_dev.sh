675e40959b92478c4114806879afaeb6724de813
suabae <bsa0322@ewhain.net>
2024-03-27 02:32:50 +0900

    👷 chore CI/CD docker install -y

--- scripts/dev-server/deploy_dev.sh
+++ scripts/dev-server/deploy_dev.sh
@@ -7,8 +7,8 @@ then
   echo "Start installing docker"
   # Add Docker's official GPG key:
   sudo apt-get update
-  sudo apt-get install ca-certificates curl
-  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo apt-get -y install ca-certificates curl
+  sudo install -y -m 0755 -d /etc/apt/keyrings
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -18,7 +18,7 @@ then
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 # Installing docker-compose if not exists
