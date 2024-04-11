3038d808504401a8cba327e900b40b4d725dccd8
bsa0322 <bsa0322@ewhain.net>
2024-04-11 13:38:28 +0900

    🎨 restruct app structure: add router

--- app/main.py
+++ app/main.py
@@ -1,52 +1,13 @@
-import httpx
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
+from .routers import models
 
 
 app = FastAPI()
 
-MODEL_SERVER_URL = "https://2795-203-255-190-41.ngrok-free.app/predict"
-INIT_BOUND = 0.3
-MIN_BOUND = 0.1
-MAX_BOUND = 0.15
+
+app.include_router(models.router)
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/adt/init-bound")
-def get_init_bound():
-    return INIT_BOUND
-
-
-@app.get("/adt/min-bound")
-def get_init_bound():
-    return MIN_BOUND
-
-
-@app.get("/adt/max-bound")
-def get_init_bound():
-    return MAX_BOUND
-
-
-@app.post("/adt")
-def drum_transcription(file: UploadFile = File(...), bpm: int = 0):
-    response = httpx.post(
-        MODEL_SERVER_URL, files={"file": (file.filename, file.file)}, timeout=None
-    )
-
-    drum_data = response.json()
-    drum_instrument = drum_data["drum_instrument"]
-    onsets_arr = drum_data["onsets_arr"]
-
-    result = []
-    for drum in drum_instrument:
-        idx = drum[0]
-        pitch = drum[1]
-        ts = onsets_arr[idx]
-        for p in pitch:
-            result.append({"pitch": p, "ts": ts})
-
-    return {"result": result}
+def connection_check():
+    return "Connection OK"
---
+++ app/routers/models.py
@@ -0,0 +1,57 @@
+import httpx
+
+from fastapi import APIRouter, File, UploadFile, HTTPException
+
+
+router = APIRouter(
+    prefix="/models",
+    tags=["models"],
+    responses={404: {"description": "Not found"}},
+)
+
+MODEL_SERVER_URL = "https://2795-203-255-190-41.ngrok-free.app/predict"
+INIT_BOUND = 0.3
+MIN_BOUND = 0.1
+MAX_BOUND = 0.15
+
+
+@router.get("/adt/init-bound")
+def get_init_bound():
+    return INIT_BOUND
+
+
+@router.get("/adt/min-bound")
+def get_min_bound():
+    return MIN_BOUND
+
+
+@router.get("/adt/max-bound")
+def get_max_bound():
+    return MAX_BOUND
+
+
+@router.post("/adt/predict")
+def drum_transcription(file: UploadFile = File(...)):
+    try:
+        response = httpx.post(
+            MODEL_SERVER_URL, files={"file": (file.filename, file.file)}, timeout=None
+        )
+    except:
+        raise HTTPException(status_code=503, detail="Model server error")
+
+    try:
+        drum_data = response.json()
+        drum_instrument = drum_data["drum_instrument"]
+        onsets_arr = drum_data["onsets_arr"]
+    except:
+        raise HTTPException(status_code=404, detail="Not Found")
+
+    result = []
+    for drum in drum_instrument:
+        idx = drum[0]
+        pitch = drum[1]
+        ts = onsets_arr[idx]
+        for p in pitch:
+            result.routerend({"pitch": p, "ts": ts})
+
+    return {"result": result}
